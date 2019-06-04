408522fed537190c8ec5cfc3ff3410d8d755de04
Jeong Tae Yong <wjdxodyd11@gmail.com>
2019-06-04 17:40:20 +0900

    Update main_js.js

---
+++ main_js.js
@@ -0,0 +1,18 @@
+var apimyKey; // my api key
+const searchFormTag = document.querySelector('.search-form'); // get form tag
+const musicSearch = document.querySelector('#music-search'); // get music-search tag
+const resultSection = document.querySelector('.results'); // get resultSection
+const audioplayer = document.querySelector('.music-player'); // get player audio tag
+const widget = document.querySelector('.widget'); // get widget div
+
+function playMusic(TrackDiv) { // 음악을 실제로 재생한다.
+
+}
+
+function search(artistName) { // 아티스트 이름으로 음악을 검색해서 가져온다.
+  //Json 사용
+}
+
+function pullTracks(Name) {
+  //Name을 가져와서 트랙들을 표시한다.
+}
